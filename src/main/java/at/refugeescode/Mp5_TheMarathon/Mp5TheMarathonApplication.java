2230fd5141d742c84ca1ff40bfcad3840f388f98
timaf <lo_sa_na@yahoo.com>
2018-03-27 06:23:18 +0200

    first step

---
+++ src/main/java/at/refugeescode/Mp5_TheMarathon/Mp5TheMarathonApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp5_TheMarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp5TheMarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp5TheMarathonApplication.class, args);
+	}
+}
